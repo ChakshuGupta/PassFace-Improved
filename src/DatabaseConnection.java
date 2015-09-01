98076c06977fdcd40aac66b27dbfbd39c606bc21
Chakshu Gupta <ChakshuGupta@users.noreply.github.com>
2015-09-01 19:31:22 +0530

    Update DatabaseConnection.java

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -84,7 +84,7 @@ public class DatabaseConnection
 			if(connection!=null)
 			{
 				statement = connection.createStatement();	
-				String sqlUserTable = "CREATE TABLE IF NOT EXISTS USERS (UserId VARCHAR(255), FirstName CHARACTER(255), LastName CHARACTER(255))";
+				String sqlUserTable = "CREATE TABLE IF NOT EXISTS USERS (UserId VARCHAR(255))";
 				String sqlPassFaceTable = "CREATE TABLE IF NOT EXISTS PassFace (UserId VARCHAR(255), PassFace1 INTEGER, PassFace2 INTEGER, PassFace3 INTEGER, Direction1 CHARACTER(255), Direction2 CHARACTER(255), Direction3 CHARACTER(255), Displacement INTEGER)";
 				statement.executeUpdate(sqlUserTable);
 				statement.executeUpdate(sqlPassFaceTable);
@@ -101,11 +101,11 @@ public class DatabaseConnection
 		}
     	
     }
-    void InsertDataInTable(String UserID, String FName, String LName, int passface[], Object dir[], Object disp[]) // INSERT DATA IN REGISTERED USER TABLE
+    void InsertDataInTable(String UserID, int passface[], Object dir[], Object disp[]) // INSERT DATA IN REGISTERED USER TABLE
     {
     	PreparedStatement preparedStatement = null;
     	PreparedStatement preparedStatement1 = null;
-    	String InsertSql = "INSERT INTO USERS(UserId, FirstName, LastName) VALUES (\" " + UserID + " \", \" " + FName + " \", \" " + LName + " \" )";
+    	String InsertSql = "INSERT INTO USERS(UserId) VALUES (\" " + UserID + " \" )";
     	String Insert = "INSERT INTO PassFace(UserId, PassFace1, PassFace2, PassFace3, Direction1,  Direction2, Direction3, Displacement) VALUES (\" " + UserID + " \",  " + passface[0] + "," + passface[1]+","+passface[2]+"  ,\"" + (String)dir[0]+"\" , \"" + (String)dir[1]+"\" , \"" + (String)dir[2] + "\" , " + disp[0]+"" + disp[1] +""+ disp[2] + " )";
     	try 
     	{
